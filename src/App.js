679f791efa067aad30cf4c5181f69dd7b9ac796e
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-03-01 13:34:36 +0000

    Create basic header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 import Api from './data/Api';
+import './styles/style.css';
 
 function App() {
   return (
     <div>
-      <p>Hello</p>
+      <Header />
       <Api />
     </div>
   );
