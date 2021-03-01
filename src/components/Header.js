93cf87b24f7634a8eee035c6e98d965e9ba7dfb4
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-03-01 22:05:16 +0000

    Basic header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Heading = styled.header`
+  background-color: var(--white);
+  box-shadow: var(--darkGrey) 0 -2px 5px;
+  h1 {
+    margin: 0;
+    padding: 10px;
+  }
+`;
 
 function Header() {
   return (
-    <div>
+    <Heading>
       <h1>Where in the world?</h1>
-    </div>
+    </Heading>
   );
 }
 
