298f43e7ded8e282ca6a4294f924a7e5d0a9e7e5
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-03-01 22:03:06 +0000

    Move api into component folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
-import Api from './data/Api';
+import Countries from './components/Countries';
 import './styles/style.css';
 
 function App() {
   return (
     <div>
       <Header />
-      <Api />
+      <Countries />
     </div>
   );
 }
