1a8c8300969aeb00000f63031e949d97ed38d4c2
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-03-02 14:26:27 +0000

    add styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,21 @@ const Heading = styled.header`
   box-shadow: var(--darkGrey) 0 -2px 5px;
   h1 {
     margin: 0;
-    padding: 10px;
+    padding: 10px 0;
+  }
+  div {
+    max-width: 1440px;
+    margin: 0 auto;
+    width: 90%;
   }
 `;
 
 function Header() {
   return (
     <Heading>
-      <h1>Where in the world?</h1>
+      <div>
+        <h1>Where in the world?</h1>
+      </div>
     </Heading>
   );
 }
