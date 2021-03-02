3c976727b2e763ad7bb68520d2a16bd387808919
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2021-03-02 14:26:41 +0000

    start creating grid template for api

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,5 +1,37 @@
 import React, { useEffect, useState } from 'react';
 
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  max-width: 1440px;
+  margin: 0 auto;
+  width: 90%;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 240px);
+  justify-content: center;
+  gap: 4rem;
+  padding: 2rem 0;
+`;
+
+const Tile = styled.div`
+  display: grid;
+  border-radius: 0.3rem;
+  box-shadow: 0 5px 10px -5px #ccc, 0 -1px 10px -5px #eee;
+  cursor: pointer;
+`;
+
+const TextWrapper = styled.div`
+  padding: 1rem 1.5rem 1.5rem;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 165px;
+`;
+
 const apiURL = 'https://restcountries.eu/rest/v2';
 
 function Api() {
@@ -29,11 +61,27 @@ function Api() {
     return <div>Loading countries...</div>;
   }
   return (
-    <ul>
-      {countries.map((country) => (
-        <li key={country.alpha3Code}>{country.name}</li>
-      ))}
-    </ul>
+    <Wrapper>
+      <Grid>
+        {countries.map((country) => (
+          <Tile>
+            <Image src={country.flag} alt={country.name} />
+            <TextWrapper>
+              <h2>{country.name}</h2>
+              <p>
+                <strong>Population:</strong> {country.population}
+              </p>
+              <p>
+                <strong>Region:</strong> {country.region}
+              </p>
+              <p>
+                <strong>Capital:</strong> {country.capital}
+              </p>
+            </TextWrapper>
+          </Tile>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
